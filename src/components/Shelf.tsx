291d5fd702c6d9511090ed271179a3e6eb39f2b3
kevinmorris1 <kdm0030@mix.wvu.edu>
2023-05-02 19:25:32 -0400

    logo fix

--- src/components/Shelf.tsx
+++ src/components/Shelf.tsx
@@ -1,15 +1,22 @@
 import { People } from "@mui/icons-material";
-import { AppBar, Paper } from "@mui/material";
+import { AppBar, Paper, Typography, useTheme } from "@mui/material";
 import React from "react";
 
 export default function Shelf() {
+  const theme = useTheme();
+
   return (
     <Paper
       square
       variant="outlined"
-      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        p: theme.spacing(0.25),
+      }}
     >
-      <img src="../../rolodex-logo.png" alt="logo" />
+      <img src={require("./rolodex.jpg")} alt="logo" style={{ height: 50 }} />
     </Paper>
   );
 }
